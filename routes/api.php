1468bd401739b4c237a1856242b5efeb6299a830
margarine <rintanaru@proton.me>
2025-05-09 11:22:08 +0700

    added register, login, logout

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [ApiController::class, 'register']);
+
+Route::post('/login', [ApiController::class, 'login']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/profile', [ApiController::class, 'profile']);
+    
+    Route::post('/logout', [ApiController::class, 'logout']);
+});
