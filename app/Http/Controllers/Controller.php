2c4ceb4076514bd0d821feb323cdf45b51d0a1e0
margarine <rintanaru@proton.me>
2025-05-27 08:58:33 +0700

    swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
+/**
+*
+* @OA\Info(
+*      version="0.0.1",
+*      title="Dekat.in API",
+*      description="API for Dekat.in, a location-based service platform"
+*  )
+* @OA\SecurityScheme(
+*     securityScheme="sanctum",
+*     type="http",
+*     scheme="bearer",
+*     bearerFormat="JWT"
+* )
+*/
 abstract class Controller
 {
     //
